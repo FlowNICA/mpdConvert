15a661dcf194ccd46c1e6353b756eaded1809ae6
PeterParfenov <terrylapard@gmail.com>
2024-03-15 14:47:33 +0300

    Added PID info for pions

--- getPID.C
+++ getPID.C
@@ -17,10 +17,12 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
   }
 
   auto h2_recDedxPq = (TH2D*)fi->Get("h2_recDedxPq");
+  auto h2_recDedxPqReverse = (TH2D*)fi->Get("h2_recDedxPqReverse");
   auto h2_recM2Pq = (TH2D*)fi->Get("h2_recM2Pq");
   auto h3_recDedxM2Pq = (TH3D*)fi->Get("h3_recDedxM2Pq");
+  auto h3_recDedxM2PqReverse = (TH3D*)fi->Get("h3_recDedxM2PqReverse");
 
-  if (!h2_recDedxPq || !h2_recM2Pq || !h3_recDedxM2Pq){
+  if (!h2_recDedxPq || !h2_recDedxPqReverse || !h2_recM2Pq || !h3_recDedxM2Pq || !h3_recDedxM2PqReverse){
     std::cerr << "Error: cannot open needed histograms!" << std::endl;
     return;
   }
@@ -29,19 +31,35 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
   h3_recDedxM2Pq->GetXaxis()->SetRangeUser(0.6, 1.2);
   h3_recDedxM2Pq->GetZaxis()->SetRangeUser(0.3, 5.);
   auto h2_recDedxPq_proton = (TH2D*)h3_recDedxM2Pq->Project3D("yz")->Clone();
+  h2_recDedxPq_proton->SetName("h2_recDedxPq_proton");
   auto p_recDedxPq_proton = (TProfile*)h2_recDedxPq_proton->ProfileX(Form("p_%s_proton", h2_recDedxPq_proton->GetName()));
+  h3_recDedxM2PqReverse->GetXaxis()->SetRangeUser(-0.05, 0.05);
+  h3_recDedxM2PqReverse->GetZaxis()->SetRangeUser(0.05, 2.5);
+  //h3_recDedxM2PqReverse->GetYaxis()->SetRangeUser(0., 1500.);
+  auto h2_recDedxPq_pip = (TH2D*)h3_recDedxM2PqReverse->Project3D("yz")->Clone();
+  h2_recDedxPq_pip->SetName("h2_recDedxPq_pip");
 
   //Prepare parametrization for the Bethe-Bloch distribution (from ALEPH, ALICE experiments)
   auto f1_dedx_proton = new TF1("f1_dedx_proton", "[0]/pow(x*x/(0.938*0.938+x*x),[3]*0.5)*( [1] + pow(x*x/(0.938*0.938+x*x),[3]*0.5)-TMath::Log([2]+pow(0.938/x,[4])))", 0.3, 5.);
+  auto f1_dedx_pip = new TF1("f1_dedx_pip", "[0]/pow(x*x/(0.13957*0.13957+x*x),[3]*0.5)*( [1] + pow(x*x/(0.13957*0.13957+x*x),[3]*0.5)-TMath::Log([2]+pow(0.13957/x,[4])))", 0.1, 2.5);
+
 
   //Fit dE/dx vs P/q distribution
   for (int i=0; i<niter; i++)
     h2_recDedxPq_proton->Fit(f1_dedx_proton,"RNQ");
 
+  for (int i=0; i<niter; i++)
+    h2_recDedxPq_pip->Fit(f1_dedx_pip,"RNQ");
+
   auto h2_recDedxNormPq = std::make_unique<TH2D>("h2_recDedxNormPq", Form("Normalized %s", h2_recDedxPq->GetTitle()),
                                                  h2_recDedxPq->GetNbinsX(), h2_recDedxPq->GetXaxis()->GetBinLowEdge(1),
                                                  h2_recDedxPq->GetXaxis()->GetBinLowEdge(h2_recDedxPq->GetNbinsX()+1),
                                                  200, -2., 2.);
+
+  auto h2_recDedxNormPq_pip = std::make_unique<TH2D>("h2_recDedxNormPq_pip", Form("Normalized %s for pions", h2_recDedxPq->GetTitle()),
+                                                     h2_recDedxPq_pip->GetNbinsX(), h2_recDedxPq_pip->GetXaxis()->GetBinLowEdge(1),
+                                                     h2_recDedxPq_pip->GetXaxis()->GetBinLowEdge(h2_recDedxPq_pip->GetNbinsX()+1),
+                                                     200, -2., 2.);
 
   auto h2_recDedxNormPq_proton = std::make_unique<TH2D>("h2_recDedxNormPq_proton", Form("Normalized %s for protons", h2_recDedxPq->GetTitle()),
                                                         h2_recDedxPq_proton->GetNbinsX(), h2_recDedxPq_proton->GetXaxis()->GetBinLowEdge(1),
@@ -56,6 +74,11 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
     h2_recDedxPq->GetRandom2(x_samp, y_samp);
     h2_recDedxNormPq->Fill(x_samp, (y_samp - f1_dedx_proton->Eval(x_samp))/f1_dedx_proton->Eval(x_samp));
   }
+  nentries = (long) h2_recDedxPq_pip->GetEntries();
+  for (long i=0; i<nentries; ++i){ 
+    h2_recDedxPq_pip->GetRandom2(x_samp, y_samp);
+    h2_recDedxNormPq_pip->Fill(x_samp, (y_samp - f1_dedx_pip->Eval(x_samp))/f1_dedx_pip->Eval(x_samp));
+  }
   nentries = (long) h2_recDedxPq_proton->GetEntries();
   for (long i=0; i<nentries; ++i){ 
     h2_recDedxPq_proton->GetRandom2(x_samp, y_samp);
@@ -64,15 +87,33 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
 
   //Prepare for normalized dE/dx fit
   std::vector<std::unique_ptr<TH1D>> vh1_dedx_p; // vector of TH1D with dedx distribution for protons
+  std::vector<std::unique_ptr<TH1D>> vh1_dedx_pip; // vector of TH1D with dedx distribution for protons
   std::vector<std::unique_ptr<TF1>> vf1_dedx_1gaus_p; // vector of TF1 1gaus for proton dedx peaks
   std::vector<std::unique_ptr<TF1>> vf1_dedx_1gaus_pip; // vector of TF1 1gaus for pi+ dedx peaks
   std::vector<std::unique_ptr<TF1>> vf1_dedx_1gaus_kap; // vector of TF1 1gaus for K+ dedx peaks
+  std::vector<double> vsigm_dedx_pip_x, vsigm_dedx_pip_y, vsigm_dedx_pip_ex, vsigm_dedx_pip_ey;  
   std::vector<double> vsigm_dedx_p_x, vsigm_dedx_p_y, vsigm_dedx_p_ex, vsigm_dedx_p_ey;
 
+  auto h2_dedxpq_pip = std::make_unique<TH2D>(*((TH2D*)h2_recDedxNormPq_pip->Clone()));
+  h2_dedxpq_pip->RebinX(5);
   auto h2_dedxpq_proton = std::make_unique<TH2D>(*((TH2D*)h2_recDedxNormPq_proton->Clone()));
   h2_dedxpq_proton->RebinX(5);
 
-  int firstbin = h2_dedxpq_proton->GetXaxis()->FindBin(0.31);
+  int firstbin = h2_dedxpq_pip->GetXaxis()->FindBin(0.11);
+  for (int i=firstbin; i<h2_dedxpq_pip->GetNbinsX(); ++i) {
+    vh1_dedx_pip.push_back( std::make_unique<TH1D>( *(h2_dedxpq_pip->ProjectionY(Form("h1_%s_pip_pqbin%i", h2_dedxpq_pip->GetName(), i-firstbin), i, i)) ) );
+    vh1_dedx_pip.at(i-firstbin)->SetTitle( Form("%s for pions for %2.2f < p/q < %2.2f GeV/c",
+                                                h2_dedxpq_pip->GetTitle(),
+                                                h2_dedxpq_pip->GetXaxis()->GetBinLowEdge(i),
+                                                h2_dedxpq_pip->GetXaxis()->GetBinUpEdge(i)) );
+
+    vf1_dedx_1gaus_pip.push_back( std::make_unique<TF1>(Form("f1_dedx_1gaus_pip_pqbin%i", i-firstbin), "gaus", -0.5, 0.5) );
+    vf1_dedx_1gaus_pip.at(i-firstbin)->SetParameter(1, 0.);
+
+    vsigm_dedx_pip_x.push_back(h2_dedxpq_pip->GetXaxis()->GetBinCenter(i));
+    vsigm_dedx_pip_ex.push_back(h2_dedxpq_pip->GetXaxis()->GetBinWidth(i)*0.5);
+  }
+  firstbin = h2_dedxpq_proton->GetXaxis()->FindBin(0.31);
   for (int i=firstbin; i<h2_dedxpq_proton->GetNbinsX(); ++i) {
     vh1_dedx_p.push_back( std::make_unique<TH1D>( *(h2_dedxpq_proton->ProjectionY(Form("h1_%s_p_pqbin%i", h2_dedxpq_proton->GetName(), i-firstbin), i, i)) ) );
     vh1_dedx_p.at(i-firstbin)->SetTitle( Form("%s for protons for %2.2f < p/q < %2.2f GeV/c",
@@ -81,11 +122,7 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
                                           h2_dedxpq_proton->GetXaxis()->GetBinUpEdge(i)) );
 
     vf1_dedx_1gaus_p.push_back( std::make_unique<TF1>(Form("f1_dedx_1gaus_p_pqbin%i", i-firstbin), "gaus", -0.5, 0.5) );
-    vf1_dedx_1gaus_pip.push_back( std::make_unique<TF1>(Form("f1_dedx_1gaus_pip_pqbin%i", i-firstbin), "gaus", -0.5, 0.5) );
-    vf1_dedx_1gaus_kap.push_back( std::make_unique<TF1>(Form("f1_dedx_1gaus_kap_pqbin%i", i-firstbin), "gaus", -0.5, 0.5) );
     vf1_dedx_1gaus_p.at(i-firstbin)->SetParameter(1, 0.);
-    vf1_dedx_1gaus_pip.at(i-firstbin)->SetParameter(1, 0.);
-    vf1_dedx_1gaus_kap.at(i-firstbin)->SetParameter(1, 0.);
 
     vsigm_dedx_p_x.push_back(h2_dedxpq_proton->GetXaxis()->GetBinCenter(i));
     vsigm_dedx_p_ex.push_back(h2_dedxpq_proton->GetXaxis()->GetBinWidth(i)*0.5);
@@ -93,8 +130,15 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
 
   // Fitting dE/dx distributions
   std::cout << "Fitting dE/dx distributions" << std::endl;
+  for (int i=0; i<vh1_dedx_pip.size(); ++i){
+    for (int k=0; k<niter; ++k){
+      vh1_dedx_pip.at(i)->Fit(&*vf1_dedx_1gaus_pip.at(i), "RNQ");
+    }
+
+    vsigm_dedx_pip_y.push_back(vf1_dedx_1gaus_pip.at(i)->GetParameter(2));
+    vsigm_dedx_pip_ey.push_back(vf1_dedx_1gaus_pip.at(i)->GetParError(2));
+  }
   for (int i=0; i<vh1_dedx_p.size(); ++i){
-    std::cout << "\tfitting bin " << i << " out of " << vh1_dedx_p.size() << std::endl;
     for (int k=0; k<niter; ++k){
       vh1_dedx_p.at(i)->Fit(&*vf1_dedx_1gaus_p.at(i), "RNQ");
     }
@@ -103,10 +147,18 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
     vsigm_dedx_p_ey.push_back(vf1_dedx_1gaus_p.at(i)->GetParError(2));
   }
 
+  auto gr_dedx_sigm_pip = std::make_unique<TGraphErrors>(vsigm_dedx_pip_x.size(),
+                                                         &vsigm_dedx_pip_x[0], &vsigm_dedx_pip_y[0],
+                                                         &vsigm_dedx_pip_ex[0], &vsigm_dedx_pip_ey[0]);
+  gr_dedx_sigm_pip->SetName("gr_dedx_sigm_pip");
+  gr_dedx_sigm_pip->SetTitle("#sigma_{dE/dx} of pions vs P/q;p/q, GeV/c;#sigma_{dE/dx}, a.u.");
+
+  auto f1_dedx_sigm_pip = std::make_unique<TF1>("f1_dedx_sigm_pip", "pol0", 0.1, 2.);
+  gr_dedx_sigm_pip->Fit(&*f1_dedx_sigm_pip, "RNQ");
+
   auto gr_dedx_sigm_p = std::make_unique<TGraphErrors>(vsigm_dedx_p_x.size(),
                                                        &vsigm_dedx_p_x[0], &vsigm_dedx_p_y[0],
                                                        &vsigm_dedx_p_ex[0], &vsigm_dedx_p_ey[0]);
-
   gr_dedx_sigm_p->SetName("gr_dedx_sigm_p");
   gr_dedx_sigm_p->SetTitle("#sigma_{dE/dx} of protons vs P/q;p/q, GeV/c;#sigma_{dE/dx}, a.u.");
 
@@ -122,6 +174,7 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
   std::vector<std::unique_ptr<TF1>> vf1_m2_2gaus_pip; // vector of TF1 2gaus for pi+ m2 peaks
   std::vector<std::unique_ptr<TF1>> vf1_m2_2gaus_kap; // vector of TF1 2gaus for K+ m2 peaks
   std::vector<std::unique_ptr<TF1>> vf1_m2_2gaus_all; // vector of TF1 2gaus for all 3 m2 peaks
+  std::vector<double> vsigm_m2_pip_x, vsigm_m2_pip_y, vsigm_m2_pip_ex, vsigm_m2_pip_ey;
   std::vector<double> vsigm_m2_p_x, vsigm_m2_p_y, vsigm_m2_p_ex, vsigm_m2_p_ey;
   
   auto h2_m2pq = (TH2D*)h2_recM2Pq->Clone();
@@ -162,6 +215,8 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
     vf1_m2_2gaus_all.at(i-firstbin)->SetParameter(13, m2_pdg.at(2));
     vf1_m2_2gaus_all.at(i-firstbin)->SetParameter(16, m2_pdg.at(2));
 
+    vsigm_m2_pip_x.push_back(h2_m2pq->GetXaxis()->GetBinCenter(i));
+    vsigm_m2_pip_ex.push_back(h2_m2pq->GetXaxis()->GetBinWidth(i)*0.5);
     vsigm_m2_p_x.push_back(h2_m2pq->GetXaxis()->GetBinCenter(i));
     vsigm_m2_p_ex.push_back(h2_m2pq->GetXaxis()->GetBinWidth(i)*0.5);
   }
@@ -213,6 +268,8 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
       vh1_m2.at(i)->Fit(&*vf1_m2_2gaus_all.at(i), "RNQ");
     }
 
+    vsigm_m2_pip_y.push_back(vf1_m2_1gaus_pip.at(i)->GetParameter(2));
+    vsigm_m2_pip_ey.push_back(vf1_m2_1gaus_pip.at(i)->GetParError(2));
     vsigm_m2_p_y.push_back(vf1_m2_1gaus_p.at(i)->GetParameter(2));
     vsigm_m2_p_ey.push_back(vf1_m2_1gaus_p.at(i)->GetParError(2));
     // vsigm_m2_p_y.push_back(vf1_m2_2gaus_p.at(i)->GetParameter(2));
@@ -221,10 +278,18 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
     // vsigm_m2_p_ey.push_back(vf1_m2_2gaus_all.at(i)->GetParError(14));
   }
 
+  auto gr_m2_sigm_pip = std::make_unique<TGraphErrors>(vsigm_m2_pip_x.size(),
+                                                       &vsigm_m2_pip_x[0], &vsigm_m2_pip_y[0],
+                                                       &vsigm_m2_pip_ex[0], &vsigm_m2_pip_ey[0]);
+  gr_m2_sigm_pip->SetName("gr_m2_sigm_pip");
+  gr_m2_sigm_pip->SetTitle("#sigma_{m^{2}} of pions vs P/q;p/q, GeV/c;#sigma_{m^{2}}, (GeV/c^{2})^{2}");
+
+  auto f1_m2_sigm_pip = std::make_unique<TF1>("f1_m2_sigm_pip", "pol2", 0.3, 2.);
+  gr_m2_sigm_pip->Fit(&*f1_m2_sigm_pip, "RNQ");
+  
   auto gr_m2_sigm_p = std::make_unique<TGraphErrors>(vsigm_m2_p_x.size(),
                                                      &vsigm_m2_p_x[0], &vsigm_m2_p_y[0],
                                                      &vsigm_m2_p_ex[0], &vsigm_m2_p_ey[0]);
-
   gr_m2_sigm_p->SetName("gr_m2_sigm_p");
   gr_m2_sigm_p->SetTitle("#sigma_{m^{2}} of protons vs P/q;p/q, GeV/c;#sigma_{m^{2}}, (GeV/c^{2})^{2}");
 
@@ -239,14 +304,23 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
 
   h2_recDedxPq->Write();
   h2_recDedxNormPq->Write();
+  h2_recDedxPq_pip->Write();
+  h2_recDedxNormPq_pip->Write();
   h2_recDedxPq_proton->Write();
   h2_recDedxNormPq_proton->Write();
+  f1_dedx_pip->Write();
+  gr_dedx_sigm_pip->Write();
+  f1_dedx_sigm_pip->Write();
   f1_dedx_proton->Write();
   gr_dedx_sigm_p->Write();
   f1_dedx_sigm_p->Write();
 
   fo->mkdir("dedx_slices");
   fo->cd("dedx_slices");
+  for (int i=0; i<vh1_dedx_pip.size(); ++i){
+    vh1_dedx_pip.at(i)->Write();
+    vf1_dedx_1gaus_pip.at(i)->Write();
+  }
   for (int i=0; i<vh1_dedx_p.size(); ++i){
     vh1_dedx_p.at(i)->Write();
     vf1_dedx_1gaus_p.at(i)->Write();
@@ -254,6 +328,8 @@ void getPID(std::string inFile_qa_step1, std::string outFile_pid)
 
   fo->cd();
   h2_recM2Pq->Write();
+  gr_m2_sigm_pip->Write();
+  f1_m2_sigm_pip->Write();
   gr_m2_sigm_p->Write();
   f1_m2_sigm_p->Write();
   
