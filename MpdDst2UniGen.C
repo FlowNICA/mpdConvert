284508c059c476e8ebb863358f70471171d3baa2
Peter Parfenov <terrylapard@gmail.com>
2025-07-23 13:48:57 +0300

    First iteration is ready; need to add Lorentz boost option

--- MpdDst2UniGen.C
+++ MpdDst2UniGen.C
@@ -4,10 +4,11 @@
 #include <TStopwatch.h>
 
 #include "FairMCEventHeader.h"
+#include "MpdMCTrack.h"
 
 R__ADD_INCLUDE_PATH($VMCWORKDIR)
 
-void MpdDst2UniGen(std::string inFileName, std::string outFileName)
+void MpdDst2UniGen(std::string inFileName, std::string outFileName, bool isBoost=false, float snn=0.)
 {
   if (inFileName == "") {
     std::cerr << "Input file path is empty! Abort..." << std::endl;
@@ -35,14 +36,16 @@ void MpdDst2UniGen(std::string inFileName, std::string outFileName)
   TFile *fo = new TFile(outFileName.c_str(), "recreate");
   URun uheader;
   UEvent *uevent = new UEvent();
-  TTree *tree = new TTree("events", generator);
-  tree->Branch("event", "UEvent", event);
+  TTree *tree = new TTree("events", "UniGen model file");
+  tree->Branch("event", "UEvent", uevent);
   
   long events = dstTree->GetEntries();
   std::cout << " Number of events in the DST file = " << events << std::endl;
 
   for (long i=0; i<events; i++){
     dstTree->GetEntry(i);
+
+    if (i%100 == 0) std::cout << "\tevent [" << i << "/" << events << "]" << std::endl;
 
     uevent->Clear();
     // fill event info:     evtID,                  impact par            psiRP        # of event steps  ev step number   ev step time
@@ -58,16 +61,23 @@ void MpdDst2UniGen(std::string inFileName, std::string outFileName)
       int status = 0, parent = 0, parentDecay = 0, mate = 0, decay = 0, child[2] = {0, 0};
       double weight = 1.;
       TLorentzVector position(mctrack->GetStartX(), mctrack->GetStartY(), mctrack->GetStartZ(), mctrack->GetStartT());
-      TLorentzVector momentum(mctrack->GetPx(), mctrack->GetPy(), mctrack->GetPz(), mctrack->GetEnergy());
+      TLorentzVector momentum;
+      if (!isBoost){
+        momentum.SetPxPyPzE(mctrack->GetPx(), mctrack->GetPy(), mctrack->GetPz(), mctrack->GetEnergy());
+      } else {
+        momentum.SetPxPyPzE(mctrack->GetPx(), mctrack->GetPy(), mctrack->GetPz(), mctrack->GetEnergy()); 
+      }
 
       UParticle uparticle(tr_counter, mctrack->GetPdgCode(), status, parent, parentDecay, mate, decay, child, momentum, position, weight);
       
       uevent->AddParticle(uparticle);
-      tree->Fill();
       tr_counter++;
     }
+    tree->Fill();
   }
   uheader.Write();
   tree->Write();
 
+  timer.Stop();
+  timer.Print();
 }
